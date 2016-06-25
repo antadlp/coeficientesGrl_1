bc43871968f5cc1ab357e80b9688d40ba14af138
antadlp <ant.adlp1986@gmail.com>
2016-06-25 08:42:48 -0500

    123

--- src/coeficientesGrl_1/CAplication.java
+++ src/coeficientesGrl_1/CAplication.java
@@ -115,51 +115,48 @@ public class CAplication {
         k = 1;
         l = 0;
         m = 0;
-        
         for(i = 1; i<=((lineaFinalMos - (numLineaMolOr + 4))); i++){
 
             Linea01MolOr = operacionesFile02.CopiarLineaIndicada(archivoGaussian, numLineaMolOr + (4 - 1) + i);
             words = Linea01MolOr.split("\\s+");
             palabras = words.length;
     //        System.out.println("verWords " + words[5] + "XX" +i);
-            for(j = (palabras - 1); j >= (((palabras - 1) - 5) + 1); j--){
-    //           words[j] = words[j].replaceAll( "\\s+","");
-    //             arreglo1[l] = Float.parseFloat(words[j]);
-    //           System.out.println(words[j]);
-                 
-                if(l == ((numMos*numMos)- (numMos % 5) - 1)){
-
-                     arreglo1[l] = words[j];
-                     operacionesFile.EscribeDeNuevo(ArchivoSalida, arreglo1[l]);
-                     operacionesFile.EscribeDeNuevo(ArchivoSalida, "\n");
+            
+            if(((numMos % 5)!= 0)^(i == ((numMos*numMos)- (numMos % 5) - 1))){
+                for(j = (palabras - 1); j >= (((palabras - 1) - (5 - (numMos % 5))) + 1); j--){
+                    arreglo1[l] = words[j];
+                    operacionesFile.EscribeDeNuevo(ArchivoSalida, arreglo1[l]);
+                    operacionesFile.EscribeDeNuevo(ArchivoSalida, "\n");
                      System.out.println(arreglo1[l]);
                      l = l + 1;
-                }else {
+                }
+                if (i == (numMos*k + m)){
+                    i = i + 3;
+                    m = m + 3;
+                    k++;
+                } 
+            } else {
+                for(j = (palabras - 1); j >= (((palabras - 1) - 5) + 1); j--){
                      arreglo1[l] = words[j];
                      operacionesFile.EscribeDeNuevo(ArchivoSalida, arreglo1[l]);
                      operacionesFile.EscribeDeNuevo(ArchivoSalida, "\n");
                      System.out.println(arreglo1[l]);
                      l = l + 1;
                 }
-                         
-                 
-             
+                if (i == (numMos*k + m)){
 
-             
-            if (i == (numMos*k + m)){
-                 
-                i = i + 3;
-                m = m + 3;
-                k++;
-                 
-            } 
-             
-             
-             
-             
-           
-             
-            }//for check
+                    i = i + 3;
+                    m = m + 3;
+                    k++;
+
+                } 
+
+
+
+
+
+
+                }//for check
                  
              
         }//for check
@@ -168,24 +165,20 @@ public class CAplication {
         
         k = 0;
         for (l=0;l<=(numMos*numMos-1); l=l+5){
-            
             if(k == ((numMos*numMos)- (numMos % 5) - 1)){
-                for(j=((int) (numMos % 5)); j>=1; j--) {
+                for(j=((int) (numMos % 5)); j>=1; j--){
                 arreglo2[k]=arreglo1[(l + j)-1];
                 operacionesFile.EscribeDeNuevo(ArchivoSalida2, arreglo2[k]);
                 operacionesFile.EscribeDeNuevo(ArchivoSalida2, "\n");
                 k++;
-                    
                 } 
             } else {
-                
                 for(j=5; j>=1; j--){
                 arreglo2[k]=arreglo1[(l + j)-1];
                 operacionesFile.EscribeDeNuevo(ArchivoSalida2, arreglo2[k]);
                 operacionesFile.EscribeDeNuevo(ArchivoSalida2, "\n");
                 k++;
                 }
-                
             }
         } //for chceck   
 
